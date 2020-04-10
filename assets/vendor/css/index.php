b9f2b2ef47af25ca7acedb52fa8437cdde985df0
Orok <derockyworld@yahoo.com>
2020-04-10 17:46:24 +0100

    added .php to all DirectoryIndex

--- assets/vendor/css/index.php
+++ assets/vendor/css/index.php
@@ -1,3 +1,3 @@
 <?php
-	header('location: ../../../not-authorized')
+	header('location: ../../../not-authorized.php')
 ?>
---
+++ not-authorized.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	ACCESS DENIED!!! <br>
+You are not permitted to view this file!
+</body>
+</html>
