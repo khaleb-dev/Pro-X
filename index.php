6f224901955a0eafb68026e27219604d9c0e527c
Orok <derockyworld@yahoo.com>
2020-04-17 15:00:39 +0100

    my new input

--- index.php
+++ index.php
@@ -5,8 +5,36 @@
 <html>
 <head>
 	<title>Index | Pro-X</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="assets/vendor/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/vendor/css/font-awesome-5.8.2/css/all.css">
+	<link rel="stylesheet" href="assets/css/index.css">
 </head>
 <body>
-<h1>PRODUCT ALERT MANAGEMENT SYSTEM</h1>
+
+	<div class="upper">
+							<ul>
+								<li><a href="#">Login</a></li>
+							</ul>
+	</div>					
+<p>welcome homw</p>
+<img class="mySlides"src="assets/img/chi.jpg">
+<img class="mySlides" src="assets/img/chi2.jpg">
+<img class="mySlides" src="assets/img/chi.jpg">
+<img class="mySlides" src="assets/img/pea.png">
+<img class="mySlides" src="assets/img/2.png">
+
+
+
+
+
+
+
+
+
+<div class="lower">
+
+
+</div>
 </body>
 </html>
