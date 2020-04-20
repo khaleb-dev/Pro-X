9451287f25b83d77f9f0a630496659da5193a765
Orok <derockyworld@yahoo.com>
2020-04-20 07:09:54 +0100

    input on index page

--- index.php
+++ index.php
@@ -11,30 +11,38 @@
 	<link rel="stylesheet" href="assets/css/index.css">
 </head>
 <body>
+<!-- NavBar -->
+<nav class="navbar main-nav fixed-top navbar-expand-lg">
+  <div class="container">
+      <a class="navbar-brand" href="#" >PEAMS</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto menu">
+        <li class="nav-item">
+          <a class="nav-link" target="_self" href="login.php">LOGIN</a>
+        </li>
+      </ul>
+      </div>
+  </div>
+</nav>
 
-	<div class="upper">
-							<ul>
-								<li><a href="#">Login</a></li>
-							</ul>
-	</div>					
-<p>welcome homw</p>
-<img class="mySlides"src="assets/img/chi.jpg">
-<img class="mySlides" src="assets/img/chi2.jpg">
-<img class="mySlides" src="assets/img/chi.jpg">
-<img class="mySlides" src="assets/img/pea.png">
-<img class="mySlides" src="assets/img/2.png">
+<!-- Banner -->
+<section class="banner bg-1 overlay">
+	<div class="container">
+		<div class="row">
+			<div class="col-12 text-center">
+				<h1>Product Expire Alert Management System</h1>
+				<h3>Keeping you Updated about your products</h3>
+				<p> <marquee style="color: #FF6600; font-family: Book Antiqua" behavior="alternate" >Always ensure to update your daily sales before leaving each day</marquee></p>
+				<a href="http://documentation.themefisher.com/docs/megakit/" class="btn btn-main-rounded">View Online Documentation</a>
+			</div>
+		</div>
+	</div>
+</section>
 
 
 
-
-
-
-
-
-
-<div class="lower">
-
-
-</div>
 </body>
 </html>
